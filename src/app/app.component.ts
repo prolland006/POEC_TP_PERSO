862b8c404423d438239fb72b64073348dbd02e94
crashmaster06 <patriceexcoffier06@gmail.com>
2016-10-05 15:18:02 +0200

    materiel design impl13;85Rmentation dans le layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,29 +14,74 @@ import { Component, ViewEncapsulation } from '@angular/core';
     require('./app.style.scss')
   ],
   template: `
-    <nav>
+<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+  <header class="mdl-layout__header">
+    <div class="mdl-layout__header-row">
+      <span class="mdl-layout-title">Gallery d'images</span>
+      <div class="mdl-layout-spacer"></div>
+    <nav class="mdl-navigation">
       <span>
-        <a [routerLink]=" ['./'] ">
-          Index
+        <a class="mdl-navigation__link" [routerLink]=" ['./'] ">
+          Accueil
         </a>
       </span>
       |
       <span>
-        <a [routerLink]=" ['./image-upload/42'] ">
+        <a class="mdl-navigation__link" [routerLink]=" ['./image-upload/42'] ">
           Upload
         </a>
       </span>
       |
       <span>
-        <a [routerLink]=" ['./images/42'] ">
-          My Images
+        <a class="mdl-navigation__link" [routerLink]=" ['./images/42'] ">
+          Mes Images
         </a>
       </span>
     </nav>
+</div>
+
+</header>
+
+
+
+    <div class="mdl-layout__drawer">
+    <nav class="mdl-navigation">
+      <span>
+        <a class="mdl-navigation__link" [routerLink]=" ['./'] ">
+          Accueil
+        </a>
+      </span>
+      <span>
+        <a class="mdl-navigation__link" [routerLink]=" ['./image-upload/42'] ">
+          Upload
+        </a>
+      </span>
+      <span>
+        <a class="mdl-navigation__link" [routerLink]=" ['./images/42'] ">
+          Mes Images
+        </a>
+      </span>
+    </nav>
+</div>
+
+
 
     <main>
       <router-outlet></router-outlet>
     </main>
+
+
+
+<footer class="mdl-mega-footer">
+  <div class="mdl-mega-footer__bottom-section">
+    <ul class="mdl-mega-footer__link-list">
+      <li><a href="#">Création de compte</a></li>
+      <li><a href="#">Connexion</a></li>
+    </ul>
+  </div>
+</footer>
+
+</div>
   `
 })
 export class AppComponent {}
