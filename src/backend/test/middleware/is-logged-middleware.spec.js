030be9aadb53f2fa4b989107191c0d24184e2283
foobar <foo@bar.com>
2016-10-05 17:00:56 +0200

    [#131152233] middleware fix

--- src/backend/test/middleware/is-logged-middleware.spec.js
+++ src/backend/test/middleware/is-logged-middleware.spec.js
@@ -8,27 +8,23 @@ describe('isLoggedMiddleware', () => {
   it('should call next if req.token is defined', (done) => {
     let req = {user: {id: '45633786', login: 'foo@bar.com', password: 'test', token: '123456789'}};
     let res = {};
-    let next = () => {
+    let next = (err) => {
+      expect(err).toBeNull();
       done();
     };
-    let err = () => {
-      throw new Error();
-    };
 
-    isLoggedMiddleware(req, res, next, err);
+    isLoggedMiddleware(req, res, next);
   });
 
   it('should call err if req.token is not defined', (done) => {
     let req = {};
     let res = {};
-    let next = () => {
-      throw new Error();
-    };
-    let err = () => {
+    let next = (err) => {
+      expect(err).not.toBeNull();
       done();
     };
 
-    isLoggedMiddleware(req, res, next, err);
+    isLoggedMiddleware(req, res, next);
   });
 
 });
