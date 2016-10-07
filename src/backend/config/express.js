3bdb245a5d07f96306b32c342f0724e1e05e1726
foobar <foo@bar.com>
2016-10-07 11:36:19 +0200

    [#131152233] fix upload image size limit

--- src/backend/config/express.js
+++ src/backend/config/express.js
@@ -25,7 +25,7 @@ module.exports = function(app, config) {
 
   // app.use(favicon(config.root + '/public/img/favicon.ico'));
   app.use(logger('dev'));
-  app.use(bodyParser.json());
+  app.use(bodyParser.json({'limit': '5mb'}));
   app.use(bodyParser.urlencoded({
     extended: true
   }));
